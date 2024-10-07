9667984d4c2475581189c4b3f27f8ed83bc4c86a
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-07 10:31:40 +0600

    added usersRouter;

--- api-chat/index.ts
+++ api-chat/index.ts
@@ -3,6 +3,7 @@ import expressWs from 'express-ws';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import config from "./config";
+import usersRouter from './routers/users';
 
 const app = express();
 expressWs(app);
@@ -11,7 +12,7 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 app.use(cors(config.corsOptions));
-
+app.use('/users', usersRouter);
 
 const run = async () => {
     await mongoose.connect(config.database);
