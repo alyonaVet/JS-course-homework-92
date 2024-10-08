241ff9ffbe4d63277c65f82b8174ff11bcc66129
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-08 10:26:07 +0600

    added Routes;

--- frontend-chat/src/App.tsx
+++ frontend-chat/src/App.tsx
@@ -1,12 +1,28 @@
 import './App.css';
+import AppToolbar from './UI/AppToolbar/AppToolbar';
+import {Route, Routes} from 'react-router-dom';
+import Register from './features/users/components/Register';
+import Login from './features/users/components/Login';
+import NotFoundPage from './UI/NotFoundPage/NotFoundPage';
+import ChatPage from './features/chat/ChatPage';
 
 const App = () => {
 
     return (
-        <>
-            Hello World!
-        </>
-    );
+      <>
+        <header>
+          <AppToolbar/>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<ChatPage />} />
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
+          </Routes>
+        </main>
+      </>
+    )
 };
 
 export default App;
