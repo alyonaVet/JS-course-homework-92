8f4e3c0e8424da0751135f8614a0596cc29e9175
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-08 18:36:40 +0600

    added messagesRouter;

--- api-chat/index.ts
+++ api-chat/index.ts
@@ -2,28 +2,31 @@ import express from 'express';
 import expressWs from 'express-ws';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import config from "./config";
+import config from './config';
 import usersRouter from './routers/users';
+import messagesRouter from './routers/messages'; // WebSocket router
 
 const app = express();
+
 expressWs(app);
 
-const port = 8000;
-app.use(cors());
 app.use(express.json());
 app.use(cors(config.corsOptions));
 app.use('/users', usersRouter);
 
+messagesRouter(app);
+
+const port = 8000;
 const run = async () => {
-    await mongoose.connect(config.database);
+  await mongoose.connect(config.database);
 
-    app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
 
-    process.on('exit', () => {
-        mongoose.disconnect();
-    });
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
 };
 
 run().catch(console.error);
